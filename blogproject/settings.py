06c38ddcc36c94cfeb84481a6cc0316b29cb12c1
shfidrla0328 <shfidrla@gmail.com>
2019-07-31 02:52:57 +0900

    third-4

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blogapp.apps.BlogappConfig',
     'portfolio.apps.PortfolioConfig',
-    'accounts.apps.AccountsConfig',
 ]
 
 MIDDLEWARE = [
