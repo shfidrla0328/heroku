49d3f1d9cdeb53feb78ab67f5c6c249f809bda78
shfidrla0328 <shfidrla@gmail.com>
2019-07-31 03:03:01 +0900

    third-7

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproject.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blogproject.settings.local')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -19,5 +19,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogproject.settings.local")
